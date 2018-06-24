965e5964cefbe6d24d542fa9088a49faca360ab3
Mateusz Konieczny <matkoniecz@gmail.com>
2018-06-24 13:44:46 +0200

    add screenshots

---
+++ geographic_data_mining_and_visualisation_for_beginners/screenshot_generation.py
@@ -0,0 +1,43 @@
+# this file is licenced under CC0 (or it is public domain anyway if content is not considered as copyrightable)
+
+# instructions for Linux using Firefox
+# download Geckodriver binary - https://github.com/mozilla/geckodriver/releases
+# put it somewhere where $PATH will find it
+
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# as recommended on http://selenium-python.readthedocs.io/waits.html#explicit-waits
+def smart_overpass_capture(url, image_file):
+    driver.get(url)
+    element = WebDriverWait(driver, 10).until_not(
+        EC.presence_of_element_located((By.CLASS_NAME, "loading"))
+        #EC.presence_of_element_located((By.ID, "aborter")) # requires additonal sleep to avoid capturing "rendering geojson" stage
+    )
+    driver.save_screenshot(image_file)
+
+firefox_options = webdriver.FirefoxOptions()
+# in production mode headless is likely to be preferrable
+# firefox_options.set_headless()
+driver = webdriver.Firefox(firefox_options=firefox_options)
+
+# supposedly captures entire page or its part, untested
+# firefox_elem = firefox_driver.find_element_by_tag_name('html')
+# firefox_elem.screenshot(<png_screenshot_file_path>)
+
+# may capture too early - then it will show stage vefore query completed
+# may capture too late and crash as apparently there will be nothing to capture
+"""
+driver.get('http://overpass-turbo.eu/s/zMi')
+time.sleep(5)
+driver.save_screenshot("hamm_playgrounds.png")
+"""
+
+try:
+    smart_overpass_capture('http://overpass-turbo.eu/s/zMi', "Hamm_-_playgrounds.png")
+    smart_overpass_capture('http://overpass-turbo.eu/s/zMQ', "Kampong_Ayer_-_everything.png")
+finally:
+    driver.quit()
