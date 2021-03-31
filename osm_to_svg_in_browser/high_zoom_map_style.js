12ca63e3e35d8eb8c7e478fdfb5679e1b07235b8
Mateusz Konieczny <matkoniecz@gmail.com>
2021-03-31 22:20:53 +0200

    publish osm_to_svg in browser on my website

---
+++ osm_to_svg_in_browser/high_zoom_map_style.js
@@ -0,0 +1,201 @@
+/*
+    lunar_assembler - tool for generating SVG files from OpenStreetMap data. Available as a website.
+    Copyright (C) 2021 Mateusz Konieczny
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, under version 3 of the
+    License only.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+var mapStyle = {
+paintOrderCompareFunction(featureFirst, featureSecond) {
+    // < 0 - First element must be placed before second
+    // 0 - Both elements is equal, do not change order.
+    // > 0 - Second element must be placed before first.
+    // https://stackoverflow.com/a/41121134/4130619
+
+    // if featureFirst should be drawn over featureSecond
+    // on top of it, hding it
+    // return 1
+
+    return mapStyle.paintOrder(featureFirst) - mapStyle.paintOrder(featureSecond);
+    // TODO to halve calculations it would be possible to map features to values,
+    // and sort that values, right? Or maybe not...
+  },
+
+paintOrder(feature) {
+    // higher values: more on top
+
+    // TODO:
+    // it WILL fail for: buildings under bridges
+    // bridges over bridges
+    // water bridges over something
+    // undeground buildings and other features undegrounds and in tunnels
+    if(feature.properties["area:highway"] != null) {
+        return 1000;
+    }
+    if(feature.properties["building"] != null) {
+        return 900;
+    }
+    if(feature.properties["barrier"] != null) {
+        return 850;
+    }
+    if(feature.properties["waterway"] != null) {
+      return 830;
+  }
+    if(feature.properties["highway"] != null) {
+        return 800;
+    }
+    if(feature.properties["man_made"] === "bridge") {
+        return 700;
+    }
+    if(feature.properties["natural"] === "water" || feature.properties["waterway"] === "riverbank") {
+      // render natural=wood below natural=water
+      return 2;
+  }
+  if(feature.properties["natural"] === "bare_rock") {
+    // render natural=wood below natural=bare_rock
+    // render water rather than underwater rocks
+    return 2;
+  }
+    if(feature.properties["leisure"] != null) {
+        // render leisure=park below natural=water or natural=wood
+        return -2;
+    }
+    return 0;
+},
+
+fillColoring(feature){
+    console.log(feature);
+    if (["Point"].includes(feature.geometry.type)) {
+      //no rendering of points, for start size seems to randomly differ
+      // and leaves ugly circles - see building=* areas
+      return "none";        
+    }
+
+    if(feature.properties["building"] != null) {
+      return "black";
+    }
+    if(["motorway", "motorway_link", "trunk", "trunk_link", "primary", "primary_link",
+      "secondary", "secondary_link", "tertiary", "tertiary_link",
+      "unclassified", "residential",
+      "service", "track", "road"].includes(feature.properties["area:highway"])) {
+      return "#555555";
+    }
+    if(["footway", "pedestrian", "path"].includes(feature.properties["area:highway"]) || (feature.properties["highway"] == "pedestrian" && feature.properties["area"] === "yes")) {
+      return "#aaaaaa";
+    }
+    if(feature.properties["area:highway"] === "cycleway" ) {
+      return "#9595b4";
+    }
+    if(feature.properties["area:highway"] === "bicycle_crossing" ) {
+      return "#bea4c1";
+    }
+    if(feature.properties["area:highway"] === "crossing" ) {
+      return "#a06060";
+    }  
+    if(feature.properties["natural"] === "water" || feature.properties["waterway"] === "riverbank") {
+      return "blue";
+    }
+    if(feature.properties["natural"] === "wood" || feature.properties["landuse"] === "forest")  {
+      return "green";
+    }
+    if(["industrial", "railway"].includes(feature.properties["landuse"])){
+        return "#efdfef";
+    }
+    if(["residential", "highway", "retail", "commercial", "garages"].includes(feature.properties["landuse"]) || ["school", "kidergarten", "university"].includes(feature.properties["amenity"])){
+        return "#efefef";
+    }
+    if(["farmland", "vineyard"].includes(feature.properties["landuse"])){
+      return "#eef0d5";
+    }
+    if(["park", "pitch"].includes(feature.properties["leisure"]) || feature.properties["landuse"] === "village_green") {
+      return "#c8facc";
+    }
+    if(["grass", "allotments", "orchard"].includes(feature.properties["landuse"]) || ["grassland", "meadow"].includes(feature.properties["natural"]) || ["garden"].includes(feature.properties["leisure"])) {
+      return "#a2ce8d";
+    }
+    if(feature.properties["man_made"] === "bridge") {
+      return "gray";
+    }
+    if(feature.properties["natural"] === "bare_rock") {
+      return "#EEE5DC";
+    }
+    return "none";
+},
+
+
+strokeColoring(feature){
+    if(["fence", "wall"].includes(feature.properties["barrier"])) {
+        return "black";
+    }
+    if(["motorway", "motorway_link", "trunk", "trunk_link", "primary", "primary_link",
+        "secondary", "secondary_link", "tertiary", "tertiary_link",
+        "unclassified", "residential",
+        "service", "track", "road"].includes(feature.properties["highway"])) {
+        return "#555555";
+      }
+      if(["footway", "pedestrian", "path", "pedestrian"].includes(feature.properties["highway"])) {
+        return "#aaaaaa";
+      }
+      if(feature.properties["highway"] === "cycleway" ) {
+        return "#9595b4";
+      }
+  
+      if(feature.properties["aeroway"] === "runway" ||  feature.properties["aeroway"] === "taxiway" ) {
+        return "purple";
+      }
+  
+      if(["rail", "disused", "tram", "subway", "narrow_gauge", "light_rail", "preserved", "construction", "miniature", "monorail"].includes(feature.properties["railway"])) {
+        return "black";
+      }
+      if(feature.properties["waterway"] != null ) {
+        return "blue";
+      }
+
+      return "none";
+  },
+  
+strokeWidth(feature){
+    if(["motorway", "motorway_link", "trunk", "trunk_link", "primary", "primary_link",
+        "secondary", "secondary_link", "tertiary", "tertiary_link",
+        "unclassified", "residential"].includes(feature.properties["highway"])) {
+        return 2;
+      }
+      if(feature.properties["aeroway"] === "runway" ) {
+        return 5;
+      }
+      if(feature.properties["waterway"] === "river" ) {
+        return 10;
+      }
+      if(feature.properties["waterway"] === "canal" ) {
+        return 7;
+      }
+      if(feature.properties["waterway"] === "stream" ) {
+        return 2;
+      }
+      if(["ditch", "drain"].includes(feature.properties["waterway"] )) {
+        return 1;
+      }
+      if(feature.properties["aeroway"] === "taxiway" ) {
+        return 2;
+      }
+      if(["rail", "disused", "tram", "subway", "narrow_gauge", "light_rail", "preserved", "construction", "miniature", "monorail"].includes(feature.properties["railway"])) {
+        return 2;
+      }
+  
+    return 1
+  },
+  
+name(feature){
+    return feature.properties.name
+  }
+}
---
+++ osm_to_svg_in_browser/lunar_assembler.js
@@ -0,0 +1,188 @@
+var map = L.map('map').setView([50.05514, 19.92824], 18);
+mapLink = 
+    '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18,
+    }).addTo(map);
+
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+    draw: {
+        polygon: false,
+        polyline: false,
+        marker: false,
+        circle: false
+    }
+});
+map.addControl(drawControl);
+
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+        corners = layer.getLatLngs();
+
+    drawnItems.addLayer(layer);
+
+    handleTriggerFromGUI(layer.getBounds());
+});
+
+async function handleTriggerFromGUI(bounds){
+    let osmJSON = await downloadOpenStreetMapData(bounds) // https://leafletjs.com/reference-1.6.0.html#latlngbounds-getcenter
+    let geoJSON = toGeoJSON(osmJSON)
+    const width=800;
+    const height=600;
+    const geoJSONRepresentingBoundaries = leafletBoundsToGeoJSONFeatureCollectionPolygon(bounds);
+    renderUsingD3(geoJSONRepresentingBoundaries, geoJSON, width, height, mapStyle); //mapStyle is defined in separate .js file, imported here
+}
+
+// TODO - there is a function to do this, right?
+        function leafletBoundsToGeoJSONFeatureCollectionPolygon(bounds) {
+            return {
+              "type": "FeatureCollection",
+              "features": [
+                {
+                  "type": "Feature",
+                  "properties": {},
+                  "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                      [
+                        [
+                          bounds.getWest(),
+                          bounds.getSouth(),
+                        ],
+                        [
+                          bounds.getEast(),
+                          bounds.getSouth(),
+                        ],
+                        [
+                          bounds.getEast(),
+                          bounds.getNorth(),
+                        ],
+                        [
+                          bounds.getWest(),
+                          bounds.getNorth(),
+                        ],
+                        [
+                          bounds.getWest(),
+                          bounds.getSouth(),
+                        ]
+                      ]
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+  
+          // downloading OSM data
+          async function downloadOpenStreetMapData(bounds){
+              // https://leafletjs.com/reference-1.6.0.html#latlngbounds-getcenter
+              query = "";
+              // note: extra filters will break data in case of some bad/poor/substandard tagging or where someone want this kind of data
+              // extra filters are useful to reduce data overload during debugging, often bug is reproducible in their presence
+              var extra_filters = "[type!=route][type!=parking_fee][type!=waterway][type!=boundary][boundary!=administrative][boundary!=religious_administration]"
+              query += "[out:json][timeout:25];nwr" + extra_filters + "(";
+              query += bounds.getSouth();
+              query += ","
+              query += bounds.getWest();
+              query += ","
+              query += bounds.getNorth();
+              query += ","
+              query += bounds.getEast();
+              query += ");";
+              query += "out body;>;out skel qt;";
+              console.log("overpass query: " + query);
+  
+              const response = await fetch("https://overpass-api.de/api/interpreter", {
+                  method: 'POST',
+                  headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                  },
+                  body: new URLSearchParams({'data': query})
+              })
+              if (response.ok) {
+                const responseData = response.json();
+                const osmJSON = responseData;
+                return osmJSON;
+              } else {
+                alert("Overpass Turbo refused to provide data, you likely exceed usage limit of that free service.")
+              }
+          } 
+          function toGeoJSON(osmJSON) {
+              let geoJSON = osmtogeojson(osmJSON);
+              return geoJSON;
+          }
+  
+  
+      // TODO: what kind of geojson is accepted here? will it crash when I pass a point here?
+      function rewind(geojson_that_is_7946_compliant_with_right_hand_winding_order) {
+        // ARGHHHHHH ARGHHHHHH ARGHHHH
+        // https://gis.stackexchange.com/questions/392452/why-d3-js-works-only-with-geojson-violating-right-hand-rule
+        // I opened https://github.com/d3/d3-shape/issues/178
+        const d3_geojson = { ...geojson_that_is_7946_compliant_with_right_hand_winding_order };
+        d3_geojson.features = d3_geojson.features.map(f =>
+          turf.rewind(f, { reverse: true })
+        );
+        //alert(JSON.stringify(d3_geojson))
+        return d3_geojson;
+      }
+  
+  
+  
+  function renderUsingD3(geoJSONRepresentingBoundaries, data_geojson, width, height, mapStyle) {
+      // rewinding is sometimes needed, sometimes not
+      // rewinding is sometimes broken in my code (at least in oce case it was borked by my bug in futher processing!), sometimes not
+      // see https://gis.stackexchange.com/questions/392452/why-d3-js-works-only-with-geojson-violating-right-hand-rule
+      // not sure what is going on here
+      console.log("data_geojson: " + JSON.stringify(data_geojson))
+      var d3_data_geojson = rewind(data_geojson);
+      var d3_geoJSONRepresentingBoundaries = rewind(geoJSONRepresentingBoundaries);
+      console.log("d3_geojson: " + JSON.stringify(d3_data_geojson))
+  
+      var projection = d3.geoMercator().fitSize([width, height], d3_geoJSONRepresentingBoundaries)
+  
+  
+      var geoGenerator = d3.geoPath()
+      .projection(projection);
+  
+      selector = '#generated_svg_within g.generated_map'
+      let generated = '<div style="background-color:white"><svg width="' + width + 'px" height="' + height + 'px">' + "\n" + '<g class="generated_map" id="generated_map"></g>' + "\n" + '</svg></div>'
+      document.getElementById('generated_svg_within').innerHTML=generated
+  
+      d3_data_geojson.features.sort(mapStyle.paintOrderCompareFunction)
+      console.log(d3_data_geojson.features)
+      update3Map(geoGenerator, d3_data_geojson, selector, mapStyle);
+      download("generated.svg", document.getElementById('generated_svg_within').innerHTML)
+    }
+    
+    function update3Map(geoGenerator, used_data, selector) {
+      var u = d3.select(selector) // should use selector
+        .selectAll('path')
+        .data(used_data.features);
+    
+      u.enter()
+        .append('path')
+        .attr('d', geoGenerator)
+        .attr("stroke", mapStyle.strokeColoring)
+          .attr("stroke-width", mapStyle.strokeWidth)
+          .attr("fill", mapStyle.fillColoring)
+          .attr("name", mapStyle.name)
+    }
+    
+    
+      function download(filename, text) {
+        var element = document.createElement('a');      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+      
+        element.style.display = 'none';
+        document.body.appendChild(element);
+      
+        element.click();
+      
+        document.body.removeChild(element);
+      }
